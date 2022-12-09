5e47e227ae3dcd9b49734d6847045ba95edfe3a4
Wes <hi@wescarr.com>
2022-12-09 16:07:47 -0500

    updated 11ty, minor homepage tweaks to intro text, removed older css

--- eleventy.js
+++ eleventy.js
@@ -1,66 +1,33 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.setTemplateFormats(["html", "pdf", "liquid", "njk", "img", "mp4", "webm", "md", "js", "jpg", "svg", "png", "webp" ]);
-
-  // Output directory: _site
-  // Copy `img/` to `_site/img`
-  // This will copy these folders to the output without modifying them at all
+  // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("posts");
-  eleventyConfig.addPassthroughCopy("work");
-  eleventyConfig.addPassthroughCopy("work/mockup");
-  eleventyConfig.addPassthroughCopy("play");
-    eleventyConfig.addPassthroughCopy({ "**/*.pdf": "play" });
+
+  // eleventyConfig.setTemplateFormats(["html", "pdf", "nks", "liquid", "njk", "img", "mp4", "webm", "md", "js", "jpg", "svg", "png", "webp" ]);
+
+
+  // eleventyConfig.addPassthroughCopy("posts");
+  // eleventyConfig.addPassthroughCopy("work");
+  // eleventyConfig.addPassthroughCopy("work/mockup");
+  // eleventyConfig.addPassthroughCopy("play");
+  // eleventyConfig.addPassthroughCopy({ "**/*.pdf": "play" });
+
+
+  return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: "njk",
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: "njk",
+
+
 };
-
-
-//Creating pretty dates
-
-const moment = require('moment');
-moment.locale('en');
-
-module.exports = function (eleventyConfig) {
- 
-  eleventyConfig.addFilter('dateIso', date => {
-    return moment(date).toISOString();
-  });
- 
-  eleventyConfig.addFilter('dateReadable', date => {
-    return moment(date).format('LL'); // E.g. May 31, 2019
-  });
-
-  // registers the excerpt shortcode with eleventy
-  eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
-
-};
-
-
-//Blog excerpts
-
-function extractExcerpt(article) {
-  if (!article.hasOwnProperty('templateContent')) {
-    console.warn('Failed to extract excerpt: Document has no property "templateContent".');
-    return null;
-  }
- 
-  let excerpt = null;
-  const content = article.templateContent;
- 
-  // The start and end separators to try and match to extract the excerpt
-  const separatorsList = [
-    { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
-    { start: '<p>', end: '</p>' }
-  ];
- 
-  separatorsList.some(separators => {
-    const startPosition = content.indexOf(separators.start);
-    const endPosition = content.lastIndexOf(separators.end);
- 
-    if (startPosition !== -1 && endPosition !== -1) {
-      excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
-      return true; // Exit out of array loop on first match
-    }
-  });
- 
-  return excerpt;
-}
