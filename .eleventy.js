51e2a865ffbdee7060a7c0edda5800128b0fa7f5
wes <hi@wescarr.com>
2023-05-05 10:46:24 -0400

    added unlinked mockup demo project

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,31 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
 
   // maintaining image directories manually for now
   eleventyConfig.addPassthroughCopy({ "project/wobblies/img": "project/wobblies/img" }); 
   eleventyConfig.addPassthroughCopy({ "blog/andy-shauf-plays-norm/img": "blog/andy-shauf-plays-norm/img" }); 
+  eleventyConfig.addPassthroughCopy({ "work/mockup-demo": "work/mockup-demo" });   
+
+  // Set the output directory for the example directory
+  eleventyConfig.addPassthroughCopy("work/mockup-demo");
+
+  // Tell 11ty not to create separate folders for files in the work/mockup-demo directory
+  eleventyConfig.setBrowserSyncConfig({
+    server: {
+      baseDir: "./_site",
+      serveStaticOptions: {
+        extensions: ['html']
+      }
+    },
+    serveStatic: [{
+      route: '/work/mockup-demo',
+      dir: 'work/mockup-demo',
+    }]
+  });
+
+ 
 
   return {
     
@@ -27,3 +48,4 @@ module.exports = function(eleventyConfig) {
   }
   
 };
+
