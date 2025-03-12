eb6654cf899f8caee91f9a8a8b59a4d63b0576e5
wes <hi@wescarr.com>
2025-03-12 17:07:56 -0400

    trying some more 11ty image plugin work
    - webp didn't make much difference on my drawings so i removed it
    - the plugin now only processes a full and thumbnail image

--- .eleventy.js
+++ .eleventy.js
@@ -1,101 +1,57 @@
-// adding plugin to allow for active navigation
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-
-// fixing how 11ty skipped spaces and new lines in tabs
-// eleventyConfig.setLibrary('md', null); // Disable Markdown rendering
 const markdownIt = require("markdown-it");
-
-// Lightbox
 const fs = require("fs");
+const path = require("path");
+const Image = require("@11ty/eleventy-img");
 
-// Using eleventy-img Plugin 
-const path = require('path');
-const Image = require('@11ty/eleventy-img');
-
-
-module.exports = function(eleventyConfig) {
-
-  // adding plugin to allow for active navigation
+module.exports = function (eleventyConfig) {
+  // Plugins
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  // allowing for more langages -- liquid is the default, and what i'm using 
+  // Template formats
   eleventyConfig.setTemplateFormats("html,liquid,njk,md");
 
-  // making dates more readable  
+  // Readable dates
   const { DateTime } = require("luxon");
   eleventyConfig.addFilter("postDate", (dateObj) => {  
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
-
-  let options = {
-    html: true,
-    breaks: false,
-    typographer: true,
-    space:false,
-    //htmlWhitespaceSensitivity:css,
-  };
-  eleventyConfig.setLibrary("md", markdownIt(options));
-
+  // Markdown config
+  eleventyConfig.setLibrary("md", markdownIt({ html: true, breaks: false, typographer: true }));
 
   // Passthrough copy settings
   const passthroughPaths = [
-    "fonts",
-    "font.woff",
-    "img",
-    "css",
-    "js",
-    "animations",
-    "js/script.js",
-    "photoswipe/",
-    "art/img",
-    "blog/snow-barn/img",
-    "blog/ims/img",
-    "blog/2024-review/img",
-    "blog/commonplace-book/img",
-    "blog/hack-day-2023/img",
-    "blog/lunch-break-sacred-places/img",
-    "blog/lunch-break-sacred-places/img/warwick",
-    "blog/red-team-blues/img",
-    "project/wobblies/img",
-    "blog/chickens-v3/img",
-    "blog/identifying-bark-in-winter/img",
-    "blog/local-logos/img",
-    "blog/non-fiction-comics/img",
+    "fonts", "font.woff", "img", "css", "js", "animations", "js/script.js", "photoswipe/", "blog/snow-barn/img", "blog/ims/img", "blog/2024-review/img", "blog/commonplace-book/img",
+    "blog/hack-day-2023/img", "blog/lunch-break-sacred-places/img", "blog/lunch-break-sacred-places/img/warwick",
+    "blog/red-team-blues/img", "project/wobblies/img", "blog/chickens-v3/img",
+    "blog/identifying-bark-in-winter/img", "blog/local-logos/img", "blog/non-fiction-comics/img",
     "work/mockup-demo"
   ];
   passthroughPaths.forEach(path => eleventyConfig.addPassthroughCopy({ [path]: path }));
 
-
-  // Add site URL globally
+  // Global data
   eleventyConfig.addGlobalData("siteUrl", "https://wescarr.com");
 
-  // preparing images for lightbox
-
+  // Image processing & collection (JPG ONLY)
+  const imageDir = "art/img/";
   eleventyConfig.addCollection("images", async function () {
-    const imageDir = "art/img/"; // Path to image folder
-    
-    // Get list of image files in the directory
-    let files = fs.readdirSync(imageDir)
-      .filter(file => /\.(jpg|jpeg|png|webp|gif)$/i.test(file));
-  
-    // Process each image and fetch width/height
+    let files = fs.readdirSync(imageDir).filter(file => /\.(jpg|png|gif)$/i.test(file));
+
     let images = [];
     for (let file of files) {
       let imagePath = path.join(imageDir, file);
-  
-      // Get image metadata using eleventy-img
-      let metadata = await Image(imagePath, {
-        widths: [null], // Adjust for any specific width ranges you want
-        formats: ["jpeg", "webp"], 
+      let options = {
+        widths: [450, null], // Thumbnail + original
+        formats: ["jpg"], // No WebP
         outputDir: "./_site/art/img/",
         urlPath: "/art/img/"
-      });
-  
-      // Get the last format (usually the best quality version)
-      let imageInfo = metadata.jpeg[metadata.jpeg.length - 1]; // Use the appropriate format
-  
-      // Push image object with width, height, and URL
+      };
+
+      // Process image
+      let metadata = await Image(imagePath, options);
+      let imageInfo = metadata.jpeg[metadata.jpeg.length - 1];
+
       images.push({
         url: imageInfo.url,
         alt: file.replace(/\.\w+$/, "").replace(/[-_]/g, " "), // Auto alt text
@@ -103,11 +59,12 @@ module.exports = function(eleventyConfig) {
         height: imageInfo.height
       });
     }
-  
+
     return images;
   });
 
-
-
-
+  // Register the image shortcode (JPG ONLY)
+  eleventyConfig.addLiquidShortcode("image", (src, alt) => {
+    return `<img src="${src}" alt="${alt}" loading="lazy">`;
+  });
 };
